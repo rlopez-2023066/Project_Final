b64de669e3c4b2e8ebc56e71958d0cfcfac057ea
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-03-03 23:28:41 -0600

    Se agrega ruta.

--- src/product/product.routes.js
+++ src/product/product.routes.js
@@ -6,7 +6,9 @@ import {
     getProductById,
     updateProduct,
     deleteProduct,
-    getProductCategory
+    getProductCategory,
+    salesProduct,
+    outStock
 } from '../product/product.controller.js'
 
 import {
@@ -27,11 +29,12 @@ const api = Router()
 api.post('/addProduct',  validateJwt, isAdmin, productValidator,  createProduct) //Agregar Producto
 api.put('/updateProduct/:id',  validateJwt, isAdmin, productValidator, updateProduct) //Actualizar Producto
 api.delete('/deleteProduct/:id',  validateJwt, isAdmin, deleteProduct) //Eliminar Producto
+api.get('/outStock', validateJwt, isAdmin, outStock) // Lista los productos que no tienen stock.
 
 //Rutas Del CLIENTE
 api.get('/listProduct', validateJwt, listProduct) //Listar todos los productos
 api.get('/getProductById/:id', validateJwt, getProductById) //Buscar por nombre
 api.get('/getProductCategory', validateJwt, getProductCategory) //Buscar Produtos por Categoria (ID BODY)
 api.get('/listCategory', validateJwt, listCategory) //Listar todas las categorias
-
+api.get('/salesProduct', validateJwt, salesProduct) //Lista todos los producotos por los mas vendidos.
 export default api;
